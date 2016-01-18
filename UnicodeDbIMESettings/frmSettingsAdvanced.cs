af1c3f1d863a2babaa0d09073dc05e727b446c76
Daniel Sim <Daniel@Daniel-PC.(none)>
2016-01-18 22:45:06 +0800

    Add missing files

---
+++ UnicodeDbIMESettings/frmSettingsAdvanced.cs
@@ -0,0 +1,369 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.IO;
+using System.Security;
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+namespace UnicodeDbIMESettings
+{
+    public partial class frmSettingsAdvanced : Form
+    {
+
+        KeyCapture activationSequence;
+        string searchKey;
+        string clipboardKey;
+
+        public const string RegistrySubKey = "Software\\UnicodeDbIME";
+
+        public frmSettingsAdvanced()
+        {
+            InitializeComponent();
+
+            LoadSettingsFromRegistry();
+            LoadLocales();
+            LoadInstalledProfiles();
+            UpdateUI();
+        }
+
+        private void LoadLocales()
+        {
+            CultureInfo[] cultures =
+                CultureInfo.GetCultures(CultureTypes.AllCultures);
+
+            foreach (CultureInfo ci in cultures) {
+                cboLocale.Items.Add(ci.EnglishName);
+            }
+        }
+        private void LoadInstalledProfiles()
+        {
+        }
+        private void LoadSettingsFromRegistry()
+        {
+            /* Default settings first */
+            activationSequence = new KeyCapture(
+                KeyCapture.TfModModifers.CONTROL |
+                KeyCapture.TfModModifers.SHIFT,
+                Keys.U
+                );
+            searchKey = "'";
+            clipboardKey = "v";
+
+            /* Read activation sequence */
+            RegistryKey rkHKCU = Microsoft.Win32.Registry.CurrentUser;
+            RegistryKey rkUDBIME;
+
+            try
+            {
+
+                rkUDBIME = rkHKCU.OpenSubKey(RegistrySubKey, false);
+                if (rkUDBIME == null)
+                    return;
+
+                if (rkUDBIME.GetValueKind("ActivationSequenceModifiers") == RegistryValueKind.DWord)
+                {
+                    int aseqmod = (int)rkUDBIME.GetValue("ActivationSequenceModifiers");
+                    activationSequence.currentModifiers = (KeyCapture.TfModModifers) (aseqmod);
+                }
+
+                if (rkUDBIME.GetValueKind("ActivationSequenceVKey") == RegistryValueKind.DWord)
+                {
+                    int aseqvk = (int)rkUDBIME.GetValue("ActivationSequenceVKey");
+                    activationSequence.currentKey = (Keys)(aseqvk);
+                }
+
+                if (rkUDBIME.GetValueKind("SearchKey") == RegistryValueKind.String)
+                {
+                    string sk = (string)rkUDBIME.GetValue("SearchKey");
+                    if (sk.Length > 0)
+                        searchKey = sk;
+                }
+
+                if (rkUDBIME.GetValueKind("ClipboardKey") == RegistryValueKind.String)
+                {
+                    string ck = (string)rkUDBIME.GetValue("ClipboardKey");
+                    if (ck.Length > 0)
+                        clipboardKey = ck;
+                }
+            }
+            catch (SecurityException sece)
+            {
+                return;
+            }
+            catch (ObjectDisposedException ode)
+            {
+                return;
+            }
+            catch (IOException ioe)
+            {
+                return;
+            }
+            catch (UnauthorizedAccessException uae)
+            {
+                return;
+            }
+        }
+
+        private void UpdateUI()
+        {
+            txtActivationSequence.Text = activationSequence.ToString();
+            txtSearchKey.Text = searchKey.Substring(0, 1);
+            txtClipboard.Text = clipboardKey.Substring(0, 1);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCapture_Click(object sender, EventArgs e)
+        {
+            txtActivationSequence.Enabled = true;
+            txtActivationSequence.Focus();
+        }
+
+        private void txtActivationSequence_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            // Accept all keys as input keys since we want to capture them
+            e.IsInputKey = true;
+        }
+
+        private void txtActivationSequence_KeyDown(object sender, KeyEventArgs e)
+        {
+            // don't let the event bubble up
+            e.Handled = true;
+            e.SuppressKeyPress = true;
+
+            /* Escape! */
+            if (e.KeyCode == Keys.Escape && !(e.Control || e.Alt || e.Shift))
+            {
+                /* No change */
+                txtActivationSequence.Text = activationSequence.ToString();
+                txtActivationSequence.Enabled = false;
+                return;
+            }
+
+            // Some other key -- get the keycode
+            Keys key = e.KeyCode & Keys.KeyCode;
+
+            activationSequence.capturingModifiers = 0;
+            if (e.Shift && key != Keys.ShiftKey)
+                activationSequence.capturingModifiers |= KeyCapture.TfModModifers.SHIFT;
+            if (e.Control && key != Keys.ControlKey)
+                activationSequence.capturingModifiers |= KeyCapture.TfModModifers.CONTROL;
+            if (e.Alt && key != Keys.Menu)
+                activationSequence.capturingModifiers |= KeyCapture.TfModModifers.ALT;
+            
+            activationSequence.capturingKey = key;
+
+            // Some proper key pressed
+            if (key != Keys.ControlKey && key != Keys.ShiftKey && key != Keys.Menu)
+            {
+                txtActivationSequence_KeyUp(null, e);
+            }
+            else // More keys may still be captured...
+            {
+                txtActivationSequence.Text = activationSequence.ToString(true);
+            }
+        }
+
+        private void txtActivationSequence_Enter(object sender, EventArgs e)
+        {
+            txtActivationSequence.Text = "";
+            btnCapture.Text = "Capturing... (ESC to cancel)";
+            activationSequence.capturingKey = 0;
+            activationSequence.capturingModifiers = 0;
+        }
+
+        class KeyCapture
+        {
+            /* From Microsoft documentation:
+             * (Apparently the Windows Key is not supported by PreserveKey)
+    TF_MOD_ALT
+    0x0001
+    Either of the ALT keys is pressed
+    TF_MOD_CONTROL
+    0x0002
+    Either of the CTRL keys is pressed
+    TF_MOD_SHIFT
+    0x0004
+    Either of the SHIFT keys is pressed
+    TF_MOD_RALT
+    0x0008
+    The right ALT key is pressed
+    TF_MOD_RCONTROL
+    0x0010
+    The right CTRL key is pressed
+    TF_MOD_RSHIFT
+    0x0020
+    The right SHIFT key is pressed
+    TF_MOD_LALT
+    0x0040
+    The left ALT key is pressed
+    TF_MOD_LCONTROL
+    0x0080
+    The left CTRL key is pressed
+    TF_MOD_LSHIFT
+    0x0100
+    The left SHIFT key is pressed
+    TF_MOD_ON_KEYUP
+    0x0200
+    The event will be fired when the key is released. Without this flag, the event is fired when the key is pressed.
+    TF_MOD_IGNORE_ALL_MODIFIER
+    0x0400 */
+
+            [System.FlagsAttribute]
+            public enum TfModModifers
+            {
+                ALT = 0x0001,
+                CONTROL = 0x0002,
+                SHIFT = 0x0004,
+                RALT = 0x0008,
+                RCONTROL = 0x0010,
+                RSHIFT = 0x0020,
+                LALT = 0x0040,
+                LCONTROL = 0x0080,
+                LSHIFT = 0x0100,
+                ON_KEYUP = 0x0200,
+                IGNORE_ALL_MODIFIER = 0x0400,
+            }
+
+            public TfModModifers currentModifiers;
+            public System.Windows.Forms.Keys currentKey;
+
+            public TfModModifers capturingModifiers;
+            public System.Windows.Forms.Keys capturingKey;
+
+            public KeyCapture(TfModModifers modifiers, Keys key)
+            {
+                this.currentModifiers = modifiers;
+                this.currentKey = key;
+            }
+
+            public override String ToString()
+            {
+                return ToString(false);
+            }
+
+            public String ToString(bool capturing)
+            {
+                TfModModifers mods = capturing ? capturingModifiers : currentModifiers;
+                Keys key = capturing ? capturingKey : currentKey;
+                StringBuilder sb = new StringBuilder();
+
+                if (mods.HasFlag(TfModModifers.CONTROL))
+                    sb.Append("Control+");
+                if (mods.HasFlag(TfModModifers.ALT))
+                    sb.Append("Alt+");
+                if (mods.HasFlag(TfModModifers.SHIFT))
+                    sb.Append("Shift+");
+
+                sb.Append(key.ToString());
+
+                return sb.ToString();
+            }
+        }
+
+        private void txtActivationSequence_KeyUp(object sender, KeyEventArgs e)
+        {
+            activationSequence.currentKey = activationSequence.capturingKey;
+            activationSequence.currentModifiers = activationSequence.capturingModifiers;
+
+            // End capture
+            txtActivationSequence.Enabled = false;
+            txtActivationSequence.Text = activationSequence.ToString();
+            btnCapture.Text = "C&apture...";
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            SaveSettings();
+            this.Close();
+        }
+
+        private void SaveSettings()
+        {
+            /* Read activation sequence */
+            RegistryKey rkHKCU = Microsoft.Win32.Registry.CurrentUser;
+            RegistryKey rkUDBIME;
+
+            try
+            {
+
+                rkUDBIME = rkHKCU.CreateSubKey(RegistrySubKey);
+                if (rkUDBIME == null)
+                    return;
+
+                rkUDBIME.SetValue("ActivationSequenceModifiers",
+                    activationSequence.currentModifiers,
+                    RegistryValueKind.DWord);
+
+                rkUDBIME.SetValue("ActivationSequenceVKey",
+                    activationSequence.currentKey,
+                    RegistryValueKind.DWord);
+
+                rkUDBIME.SetValue("SearchKey",
+                    searchKey.Substring(0, Math.Min(searchKey.Length, 1) ),
+                    RegistryValueKind.String);
+                
+                rkUDBIME.SetValue("ClipboardKey",
+                    clipboardKey.Substring(0, Math.Min(clipboardKey.Length, 1) ),
+                    RegistryValueKind.String);
+            }
+            catch (SecurityException sece)
+            {
+                return;
+            }
+            catch (ObjectDisposedException ode)
+            {
+                return;
+            }
+            catch (IOException ioe)
+            {
+                return;
+            }
+            catch (UnauthorizedAccessException uae)
+            {
+                return;
+            }
+            /* Manipulate the registry here */
+        }
+
+        private void txtSearchKey_TextChanged(object sender, EventArgs e)
+        {
+            searchKey = txtSearchKey.Text;
+        }
+
+        private void txtClipboard_TextChanged(object sender, EventArgs e)
+        {
+            clipboardKey = txtClipboard.Text;
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    [ComImport, Guid(ProfileMgrCLSID)]
+    class InputProcessorProfilesMgr
+    {
+    }
+}
---
+++ UnicodeDbIMESettings/frmSettingsBasic.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace UnicodeDbIMESettings
+{
+    partial class frmSettingsBasic
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtActivationSequence = new System.Windows.Forms.TextBox();
+            this.btnCapture = new System.Windows.Forms.Button();
+            this.lblSearchCharacter = new System.Windows.Forms.Label();
+            this.txtSearchKey = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtClipboard = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnOK
+            // 
+            this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOK.Location = new System.Drawing.Point(331, 168);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(95, 28);
+            this.btnOK.TabIndex = 7;
+            this.btnOK.Text = "&OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(432, 168);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(95, 28);
+            this.btnCancel.TabIndex = 8;
+            this.btnCancel.Text = "&Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Unicode input activation sequence";
+            // 
+            // txtActivationSequence
+            // 
+            this.txtActivationSequence.Enabled = false;
+            this.txtActivationSequence.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.txtActivationSequence.Location = new System.Drawing.Point(209, 3);
+            this.txtActivationSequence.Name = "txtActivationSequence";
+            this.txtActivationSequence.Size = new System.Drawing.Size(146, 20);
+            this.txtActivationSequence.TabIndex = 11;
+            // 
+            // btnCapture
+            // 
+            this.btnCapture.Location = new System.Drawing.Point(375, 3);
+            this.btnCapture.Name = "btnCapture";
+            this.btnCapture.Size = new System.Drawing.Size(129, 31);
+            this.btnCapture.TabIndex = 12;
+            this.btnCapture.Text = "C&apture...";
+            this.btnCapture.UseVisualStyleBackColor = true;
+            // 
+            // lblSearchCharacter
+            // 
+            this.lblSearchCharacter.AutoSize = true;
+            this.lblSearchCharacter.Location = new System.Drawing.Point(3, 37);
+            this.lblSearchCharacter.Name = "lblSearchCharacter";
+            this.lblSearchCharacter.Size = new System.Drawing.Size(138, 13);
+            this.lblSearchCharacter.TabIndex = 13;
+            this.lblSearchCharacter.Text = "&Search activation character";
+            // 
+            // txtSearchKey
+            // 
+            this.txtSearchKey.Location = new System.Drawing.Point(209, 40);
+            this.txtSearchKey.MaxLength = 1;
+            this.txtSearchKey.Name = "txtSearchKey";
+            this.txtSearchKey.Size = new System.Drawing.Size(146, 20);
+            this.txtSearchKey.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(199, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Clip&board inspection activation character";
+            // 
+            // txtClipboard
+            // 
+            this.txtClipboard.Location = new System.Drawing.Point(209, 63);
+            this.txtClipboard.MaxLength = 1;
+            this.txtClipboard.Name = "txtClipboard";
+            this.txtClipboard.Size = new System.Drawing.Size(146, 20);
+            this.txtClipboard.TabIndex = 16;
+            this.txtClipboard.TextChanged += new System.EventHandler(this.txtClipboard_TextChanged);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 55.37634F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 44.62366F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 137F));
+            this.tableLayoutPanel1.Controls.Add(this.txtClipboard, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.txtSearchKey, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblSearchCharacter, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnCapture, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtActivationSequence, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(6, 19);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 61.79775F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 38.20225F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(510, 87);
+            this.tableLayoutPanel1.TabIndex = 18;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tableLayoutPanel1);
+            this.groupBox2.Location = new System.Drawing.Point(11, 10);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(523, 119);
+            this.groupBox2.TabIndex = 19;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Activation Keys";
+            // 
+            // frmSettingsAdvanced
+            // 
+            this.AcceptButton = this.btnOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(540, 215);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Name = "frmSettingsAdvanced";
+            this.Text = "Unicode DB IME Settings";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtActivationSequence;
+        private System.Windows.Forms.Button btnCapture;
+        private System.Windows.Forms.Label lblSearchCharacter;
+        private System.Windows.Forms.TextBox txtSearchKey;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtClipboard;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
